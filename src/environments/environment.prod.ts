7a4656bf30e02f79f97809c3c1d84fd5d9c65896
JordanR17 <jordanrodriguez1707@gmail.com>
2025-06-24 09:22:38 -0500

    FIX URL BACK

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true, // Esto es clave para la construcción de producción
-  apiUrl: 'https://restapi-web-production.up.railway.app'
+  apiUrl: 'https://sanjose-back.onrender.com'
 };
