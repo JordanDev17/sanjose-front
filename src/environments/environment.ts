47876b16370f9577c7443c4db3a90329a72f8b98
JordanR17 <jordanrodriguez1707@gmail.com>
2025-06-24 09:38:52 -0500

    url back

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 // src/environments/environment.ts
 export const environment = {
   production: false,
-  apiUrl: 'https://sanjose-back.onrender.com' // O el puerto local donde corres tu backend
+  apiUrl: 'https://sanjose-back.onrender.com/' // O el puerto local donde corres tu backend
   // apiUrl: 'http://localhost:3000/'
 };
