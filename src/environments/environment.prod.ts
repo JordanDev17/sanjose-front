f998e396b082b58e7642bbd02b4211799cd71f88
JordanR17 <jordanrodriguez1707@gmail.com>
2025-07-03 11:45:15 -0500

    URL Backend Railway

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 // src/environments/environment.prod.ts
 export const environment = {
   production: true, // Esto es clave para la construcción de producción
-  apiUrl: 'https://restapi-web-production.up.railway.app/'
+  apiUrl: 'https://sanjose-back-production.up.railway.app/',
+  openWeatherApiKey: 'dc846fda6c957902214dcc7ac40752ef', // clave API de OpenWeatherMap
 };
